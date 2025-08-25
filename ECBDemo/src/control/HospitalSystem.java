e4c2223b94b1b2038ef102be51263e01c7249425
jimmyytai0619 <taicy-wm24@student.tarc.edu.my>
2025-08-25 16:27:46 +0800

    Add files via upload

---
+++ ECBDemo/src/control/HospitalSystem.java
@@ -0,0 +1,29 @@
+package control;
+
+import adt.ArrayQueue;
+import adt.QueueInterface;
+import entity.Patient;
+
+public class HospitalSystem {
+    private QueueInterface<Patient> waitingQueue;
+
+    public HospitalSystem() {
+        waitingQueue = new ArrayQueue<>();
+    }
+
+    public void registerPatient(Patient p) {
+        waitingQueue.enqueue(p);
+    }
+
+    public Patient callNextPatient() {
+        return waitingQueue.dequeue();
+    }
+
+    public Patient peekNextPatient() {
+        return waitingQueue.getFront();
+    }
+
+    public boolean isQueueEmpty() {
+        return waitingQueue.isEmpty();
+    }
+}
