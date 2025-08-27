0c1d976ceb14ac88ace00b4d7d75562924b18f6f
Evan Chua <87073287+Evan0428@users.noreply.github.com>
2025-08-28 02:30:38 +0800

    Add files via upload

--- ECBDemo/src/entity/Patient.java
+++ ECBDemo/src/entity/Patient.java
@@ -1,32 +1,31 @@
 package entity;
 
-import adt.ArrayList;
-import adt.ListInterface;
-
+/**
+ * Author: Your Name
+ */
 public class Patient {
-    private String name;
-    private int age;
-    private String id;
-    private ListInterface<Treatment> treatments;
+	private final String patientId;
+	private String name;
+	private String phone;
+	private String email;
 
-    public Patient(String name, int age, String id) {
-        this.name = name;
-        this.age = age;
-        this.id = id;
-        this.treatments = new ArrayList<>();
-    }
+	public Patient(String patientId, String name, String phone, String email) {
+		this.patientId = patientId;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+	}
 
-    public String getName() { return name; }
-    public int getAge() { return age; }
-    public String getId() { return id; }
-    public ListInterface<Treatment> getTreatments() { return treatments; }
+	public String getPatientId() { return patientId; }
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+	public String getPhone() { return phone; }
+	public void setPhone(String phone) { this.phone = phone; }
+	public String getEmail() { return email; }
+	public void setEmail(String email) { this.email = email; }
 
-    public void addTreatment(Treatment treatment) {
-        treatments.add(treatment);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("ID: %s | Name: %s | Age: %d", id, name, age);
-    }
+	@Override
+	public String toString() {
+		return patientId + " - " + name;
+	}
 }
