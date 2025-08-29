0886c2cf4f43066bd2afd9c607680da6f08044bb
LTH0303 <tianhong018@gmail.com>
2025-08-29 10:37:47 +0800

    Update PatientRecord.java

--- 3.0.0/src/entity/PatientRecord.java
+++ 3.0.0/src/entity/PatientRecord.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package entity;
 
-import entity.Patient;
 import java.io.Serializable;
 
 public class PatientRecord extends Patient implements Serializable {
     private String medicalHistory;
     private String allergies;
     private String contactNumber;
-    
-    public PatientRecord(String name, int id, String treatmentType, 
-                        String medicalHistory, String allergies, String contactNumber) {
-        super(name, id, treatmentType);
+
+    public PatientRecord(String id, String name, int age, String treatmentType,
+                         String medicalHistory, String allergies, String contactNumber) {
+        super(id, name, age, "", ""); 
         this.medicalHistory = medicalHistory;
         this.allergies = allergies;
         this.contactNumber = contactNumber;
@@ -23,17 +18,17 @@ public class PatientRecord extends Patient implements Serializable {
     // Getters and setters
     public String getMedicalHistory() { return medicalHistory; }
     public void setMedicalHistory(String medicalHistory) { this.medicalHistory = medicalHistory; }
-    
+
     public String getAllergies() { return allergies; }
     public void setAllergies(String allergies) { this.allergies = allergies; }
-    
+
     public String getContactNumber() { return contactNumber; }
     public void setContactNumber(String contactNumber) { this.contactNumber = contactNumber; }
 
     @Override
     public String toString() {
-        return super.toString() + 
-               String.format("\nMedical History: %s\nAllergies: %s\nContact: %s", 
-               medicalHistory, allergies, contactNumber);
+        return super.toString() +
+                String.format("\nMedical History: %s\nAllergies: %s\nContact: %s",
+                        medicalHistory, allergies, contactNumber);
     }
 }
