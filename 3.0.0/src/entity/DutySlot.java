dd62e322b9af9c12990f24f4a8a8d125c30b332a
Junzz11 <limkahjun64@gmail.com>
2025-09-29 04:18:39 +0800

    Update DutySlot.java

--- 3.0.0/src/entity/DutySlot.java
+++ 3.0.0/src/entity/DutySlot.java
@@ -1,25 +1,26 @@
 package entity;
 
 import java.io.Serializable;
-import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class DutySlot implements Serializable {
-    private final DayOfWeek day;
+    private final LocalDate date;   
     private final LocalTime start;
     private final LocalTime end;
 
-    public DutySlot(DayOfWeek day, LocalTime start, LocalTime end) {
-        this.day = day;
+    public DutySlot(LocalDate date, LocalTime start, LocalTime end) {
+        this.date = date;
         this.start = start;
         this.end = end;
     }
 
-    public DayOfWeek getDay()   { return day; }
-    public LocalTime  getStart(){ return start; }
-    public LocalTime  getEnd()  { return end; }
+    public LocalDate getDate() { return date; }
+    public LocalTime getStart() { return start; }
+    public LocalTime getEnd() { return end; }
 
-    @Override public String toString() {
-        return day + " " + start + "-" + end;
+    @Override
+    public String toString() {
+        return date + " " + start + "-" + end;
     }
 }
